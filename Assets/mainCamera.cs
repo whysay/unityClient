c20d6588de02f27a5bde9ba3f36254f52d6ad79a
whysay <gal_yong@naver.com>
2014-07-25 15:21:35 +0900

    유니티클라이언트1
    스터디용

---
+++ Assets/mainCamera.cs
@@ -0,0 +1,381 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System;
+using System.IO;
+using System.Threading;
+using System.Runtime.InteropServices;
+using System.Text;
+
+using System.Net.Json;
+
+// State object for receiving data from remote device.
+public class StateObject
+{
+	// Client socket.
+	public Socket workSocket = null;
+	// Size of receive buffer.
+	public const int BufferSize = 60000;
+	// Receive buffer.
+	public byte[] buffer = new byte[BufferSize];
+	public int recvSize = 0;
+	// Received data string.
+	//public StringBuilder sb = new StringBuilder();
+}
+
+public class mainCamera : MonoBehaviour {
+	
+	//private DataSet _ds;
+	public static bool m_isDisconnected = false;
+	
+	[Serializable]
+	
+	public struct _TCP_PACKET_H
+	{
+		public int PacketSize;
+		public short Cmd;
+	}
+	
+	
+	
+	//const int TCP_HEADER_SIZE = Marshal.SizeOf(_TCP_PACKET_H);
+	
+	//const int MAX_IOCP_BUFFER_SIZE	= 8192;			// IOCP에 사용할 버퍼의 크기
+	//const int MAX_SEND_PACKET_SIZE	= 1024;			// 가장 큰 패킷의 크기
+	
+	[StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi)]
+	struct _TMP_PACKET
+	{
+		public _TCP_PACKET_H Header;
+		[MarshalAs(UnmanagedType.ByValTStr, SizeConst = 50000)]
+		public string str;
+	};
+	public static void StructToBytes(object obj, ref byte[] packet)
+	{
+		int size = Marshal.SizeOf(obj);
+		packet = new byte[size];
+		IntPtr buffer = Marshal.AllocHGlobal(packet.Length + 1);
+		Marshal.StructureToPtr(obj, buffer, false); // 사이즈가 1이라도 틀리면 예외발생하니 주의
+		Marshal.Copy(buffer, packet, 0, packet.Length);
+		Marshal.FreeHGlobal(buffer);
+	}
+	
+	private static bool ReadStruct<T>(byte[] buffer, out T obj) where T : struct
+	{
+		obj = default(T);
+		int size = Marshal.SizeOf(typeof(T));
+		if (size > buffer.Length)
+		{
+			return false;
+		}
+		//throw new Exception();
+		IntPtr ptr = Marshal.AllocHGlobal(size);
+		Marshal.Copy(buffer, 0, ptr, size);
+		obj = (T)Marshal.PtrToStructure(ptr, typeof(T));
+		Marshal.FreeHGlobal(ptr);
+		return true;
+	}
+	
+	//---------------------------네트워크----------------------------------
+	// The port number for the remote device.
+	private const int port = 50000;
+	
+	// ManualResetEvent instances signal completion.
+	private static ManualResetEvent connectDone =
+		new ManualResetEvent(false);
+	private static ManualResetEvent sendDone =
+		new ManualResetEvent(false);
+	private static ManualResetEvent receiveDone =
+		new ManualResetEvent(false);
+	
+	// The response from the remote device.
+	private static String _response = String.Empty;
+	
+	private static void Receive(Socket client)
+	{
+		try
+		{
+			// Create the state object.
+			StateObject state = new StateObject();
+			state.workSocket = client;
+			
+			// Begin receiving the data from the remote device.
+			client.BeginReceive(state.buffer, 0, StateObject.BufferSize, 0,
+			                    new AsyncCallback(ReceiveCallback), state);
+		}
+		catch (Exception e)
+		{
+			Console.WriteLine(e.ToString());
+		}
+	}
+	
+	private static void ReceiveCallback(IAsyncResult ar)
+	{
+		try
+		{
+			// Retrieve the state object and the client socket 
+			// from the asynchronous state object.
+			StateObject state = (StateObject)ar.AsyncState;
+			Socket client = state.workSocket;
+			
+			// Read data from the remote device.
+			int bytesRead = client.EndReceive(ar);
+			
+			if (bytesRead > 0)
+			{
+				// There might be more data, so store the data received so far.
+				//state.sb.Append(Encoding.ASCII.GetString(state.buffer, 0, bytesRead));
+				
+				// 스트링 버퍼말고 바이트버퍼를 따로 만들어서 패킷 1개 처리 완료될때까지 모아서
+				// 처리하도록 제작되야함
+				
+				//byte[] packetData = Encoding.ASCII.GetBytes(state.sb.ToString());
+				byte[] packetData = state.buffer;
+				_TCP_PACKET_H recvheader;
+				if (ReadStruct<_TCP_PACKET_H>(state.buffer, out recvheader))
+				{
+					state.recvSize += bytesRead;
+					if (state.recvSize < recvheader.PacketSize)
+					{
+						// 아직 덜받음
+						client.BeginReceive(state.buffer, state.recvSize, recvheader.PacketSize - state.recvSize, 0,
+						                    new AsyncCallback(ReceiveCallback), state);
+						return;
+						
+					}
+					// 패킷헤더 사이즈가 이상하게 나옴 디버깅 필요..
+					int strDataSize = recvheader.PacketSize - Marshal.SizeOf(recvheader);
+					byte[] strData = new byte[strDataSize];
+
+					Array.Copy(state.buffer, Marshal.SizeOf(recvheader),strData, 0, strDataSize);
+					//arrCopy(state.buffer, Marshal.SizeOf(recvheader), strData, 0, strDataSize);
+					lock (lockObject)
+					{
+						packetStr.Enqueue(Encoding.ASCII.GetString(strData));
+					}
+					//state.sb.Clear();
+				}
+				
+				// Get the rest of the data.
+				state.recvSize = 0;
+				client.BeginReceive(state.buffer, 0, StateObject.BufferSize, 0,
+				                    new AsyncCallback(ReceiveCallback), state);
+				
+			}
+			else
+			{
+				// All the data has arrived; put it in response.
+				//if (state.sb.Length > 1)
+				//{
+				//    _response = state.sb.ToString();
+				//}
+				m_isDisconnected = true;
+				// Signal that all bytes have been received.
+				receiveDone.Set();
+			}
+		}
+		catch (Exception e)
+		{
+			m_isDisconnected = true;
+			receiveDone.Set();
+			//Console.WriteLine(e.ToString());
+		}
+	}
+	
+	private static void Send(Socket client, byte[] byteData, int size)
+	{
+		// Convert the string data to byte data using ASCII encoding.
+		// byte[] byteData = Encoding.ASCII.GetBytes(data);
+		
+		// Begin sending the data to the remote device.
+		client.BeginSend(byteData, 0, size, 0,
+		                 new AsyncCallback(SendCallback), client);
+	}
+	
+	private static void SendCallback(IAsyncResult ar)
+	{
+		try
+		{
+			// Retrieve the socket from the state object.
+			Socket client = (Socket)ar.AsyncState;
+			
+			// Complete sending the data to the remote device.
+			int bytesSent = client.EndSend(ar);
+			Console.WriteLine("Sent {0} bytes to server.", bytesSent);
+			
+			// Signal that all bytes have been sent.
+			sendDone.Set();
+		}
+		catch (Exception e)
+		{
+			Console.WriteLine(e.ToString());
+		}
+	}
+	//---------------------------------------------------------------------
+	//static AsynchronousClient _client;
+	//TcpClient client = null;
+	//static NetworkStream netStream = null;
+	static Socket _client;
+	static Queue<string> packetStr = new Queue<string>();
+	static object lockObject = new object();
+	
+	public static void NetCallbackFunc()
+	{
+		while(true)
+		{
+			Receive(_client);
+			receiveDone.WaitOne();
+			if (m_isDisconnected)
+				break;
+		}
+	}
+
+
+	// Use this for initialization
+	void Start () {
+		//Sample ();
+		Connect ();
+
+//		string reqStr;
+//		JsonObjectCollection collection = new JsonObjectCollection();
+//		collection.Add(new JsonNumericValue("stars", 1));
+//		reqStr = collection.ToString();
+//		infoReq(3, reqStr);
+	}
+	
+	
+	// Update is called once per frame
+	void Update () {
+		packetProc ();
+
+	}
+	
+	
+	static void Connect() 
+	{
+		try
+		{
+			_client = new Socket(AddressFamily.InterNetwork,
+			                     SocketType.Stream, ProtocolType.Tcp);
+			
+			// Connect to the remote endpoint.
+			_client.Connect("127.0.0.1", port);
+			
+			Thread netThread = new Thread(new ThreadStart(NetCallbackFunc));
+			netThread.Start();
+			
+			// 로그인창 띄우기
+			//ShowLoginWindow();
+		}
+		catch (Exception e)
+		{
+			Console.WriteLine(e.Message);
+		}
+		finally
+		{
+			
+		}
+	}
+
+	private void packetProc()
+	{
+		if (m_isDisconnected)
+		{
+			//Close();
+			return;
+		}
+		string curStr;
+		lock (lockObject)
+		{
+			if (0 == packetStr.Count)
+				return;
+			
+			curStr = packetStr.Dequeue();
+			
+			// code goes here
+			JsonTextParser parser = new JsonTextParser();
+			
+			
+			JsonObject obj = parser.Parse(curStr);
+			
+			JsonObjectCollection col = (JsonObjectCollection)obj;
+			
+			switch (Convert.ToInt32(col["cmd"].GetValue()))
+			{
+			case 1:// 로그인결과
+			{
+				switch (Convert.ToInt32(col["ret"].GetValue()))
+				{
+				case 1:// 없는유저
+				{
+//					string reqStr;
+//					JsonObjectCollection collection = new JsonObjectCollection();
+//					collection.Add(new JsonNumericValue("stars", 1));
+//					reqStr = collection.ToString();
+//					infoReq(3, reqStr);
+				}
+					break;
+				case 2:// 비번틀림
+				{
+
+				}
+					break;
+				case 0:// 로그인성공
+				{
+					string reqStr;
+					JsonObjectCollection collection = new JsonObjectCollection();
+					collection.Add(new JsonNumericValue("stars", 1));
+					reqStr = collection.ToString();
+					infoReq(3, reqStr);
+				}
+					break;
+				}
+			}
+				break;
+			}//switch
+		}
+	}
+
+	private void infoReq(short reqType, string inputStr)
+	{
+		//로그인
+		_TMP_PACKET aaa = new _TMP_PACKET();
+		aaa.str = inputStr;
+		int size = Marshal.SizeOf(aaa.Header);
+		if (inputStr == "")
+			aaa.Header.PacketSize = (short)(size);
+		else
+			aaa.Header.PacketSize = (short)(size + Encoding.Unicode.GetBytes(aaa.str).Length + 2);
+		aaa.Header.Cmd = reqType;//요청타입
+		byte[] byteBuffer = new byte[aaa.Header.PacketSize];
+		StructToBytes(aaa, ref byteBuffer);
+		
+		Send(_client, byteBuffer, aaa.Header.PacketSize);
+	}
+
+	void OnGUI() 
+	{
+		Event e = Event.current;
+		if (e.button == 0 && e.isMouse)
+		{
+			Debug.Log ("Left Click & send");
+			string reqStr;
+			JsonObjectCollection collection = new JsonObjectCollection ();
+			collection.Add (new JsonNumericValue ("stars", 1));
+			reqStr = collection.ToString ();
+			infoReq (3, reqStr);
+		}
+		else
+			if (e.button == 1)
+				Debug.Log("Right Click");
+		else
+			if (e.button == 2)
+				Debug.Log("Middle Click");
+		else
+			if (e.button > 2)
+				Debug.Log("Another button in the mouse clicked");
+		
+	}
+	
+}
---
+++ Assets/touch.cs
@@ -0,0 +1,95 @@
+﻿//using UnityEngine;
+//using System.Collections;
+//
+//public class bside001 : MonoBehaviour {
+//	
+//	delegate void listener( string type, int id, float x, float y, float dx, float dy );
+//	event listener begin0, begin1, begin2, begin3, begin4;
+//	event listener move0, move1, move2, move3, move4;
+//	event listener end0, end1, end2, end3, end4;
+//	
+//	Vector2[] delta = new Vector2[5];
+//	
+//	void Update(){
+//		
+//		int count = Input.touchCount;
+//		if( count == 0 ) return;
+//		
+//		for( int i = 0 ; i < count ; i++ ){
+//			Touch touch = Input.GetTouch(i);
+//			int id = touch.fingerId;
+//			Vector2 pos = touch.position;
+//			if( touch.phase == TouchPhase.Began ) delta[id] = touch.position;
+//			float x, y, dx, dy;
+//			x = pos.x;
+//			y = pos.y;
+//			if( touch.phase == TouchPhase.Began ){
+//				dx = dy = 0;
+//			}else{
+//				dx = pos.x - delta[id].x;
+//				dy = pos.y - delta[id].y;
+//			}
+//			
+//			//상태에 따라 이벤트를 호출하자
+//			if( touch.phase == TouchPhase.Began ){
+//				switch( id ){
+//				case 0: if( begin0 != null ) begin0( "begin", id, x, y, dx, dy ); break;
+//				case 1: if( begin1 != null ) begin1( "begin", id, x, y, dx, dy ); break;
+//				case 2: if( begin2 != null ) begin2( "begin", id, x, y, dx, dy ); break;
+//				case 3: if( begin3 != null ) begin3( "begin", id, x, y, dx, dy ); break;
+//				case 4: if( begin4 != null ) begin4( "begin", id, x, y, dx, dy ); break;
+//				}
+//			}else if( touch.phase == TouchPhase.Moved ){
+//				switch( id ){
+//				case 0: if( move0 != null ) move0( "move", id, x, y, dx, dy ); break;
+//				case 1: if( move1 != null ) move1( "move", id, x, y, dx, dy ); break;
+//				case 2: if( move2 != null ) move2( "move", id, x, y, dx, dy ); break;
+//				case 3: if( move3 != null ) move3( "move", id, x, y, dx, dy ); break;
+//				case 4: if( move4 != null ) move4( "move", id, x, y, dx, dy ); break;
+//				}
+//			}else if( touch.phase == TouchPhase.Ended ){
+//				switch( id ){
+//				case 0: if( end0 != null ) end0( "end", id, x, y, dx, dy ); break;
+//				case 1: if( end1 != null ) end1( "end", id, x, y, dx, dy ); break;
+//				case 2: if( end2 != null ) end2( "end", id, x, y, dx, dy ); break;
+//				case 3: if( end3 != null ) end3( "end", id, x, y, dx, dy ); break;
+//				case 4: if( end4 != null ) end4( "end", id, x, y, dx, dy ); break;
+//				}
+//			}
+//		}
+//	}
+//	void Start(){
+//		begin0 += onTouch;
+//		end0 += onTouch;
+//		move0 += onTouch;
+//	}
+//	
+//	void onTouch( string type, int id, float x, float y, float dx, float dy){
+//		switch( type ){
+//		case"begin": Debug.Log( "down:" + x + "," + y ); break;
+//		case"end": Debug.Log( "end:" + x + "," + y +", d:" + dx +","+dy ); break;
+//		case"move": Debug.Log( "move:" + x + "," + y +", d:" + dx +","+dy ); break;
+//		}
+//	}
+//}
+
+//using UnityEngine;
+//using System.Collections;
+//
+//public class ExampleClass : MonoBehaviour {
+//	void OnGUI() {
+//		Event e = Event.current;
+//		if (e.button == 0 && e.isMouse)
+//			Debug.Log("Left Click");
+//		else
+//			if (e.button == 1)
+//				Debug.Log("Right Click");
+//		else
+//			if (e.button == 2)
+//				Debug.Log("Middle Click");
+//		else
+//			if (e.button > 2)
+//				Debug.Log("Another button in the mouse clicked");
+//		
+//	}
+//}
